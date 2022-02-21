f84cfca32afb506364e6579d0538d7b69e8d4bf4
Abdusamadov-Sardor <sardorabdusamatov@750gmail.com>
2022-02-21 10:13:34 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+alert( "Assalomu alaykum dam olishga yaxshi borib keling");
+
+var sum = Number(prompt("Pul miqdorini kiriting")) ;
+
+var Dollar  = 750;
+var evro = 120;
+
+var allsum = Dollar * 10900;
+var allevro = evro * 12272.64;
+
+var allmoney = allsum + allevro;
+
+if(sum >= allmoney ){
+  console.log('${sum} Oq yol Alisher !');
+}else{
+console.log('${sum} puling kamku Alisher keyingi safarga qoldir');
+}
+
+// important sum >= 9 647 716.8 so'm
